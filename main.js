46773215ec74017c0715cfbc50ba313077b6b170
HanHanTran <tranthihan2705@gmail.com>
2022-06-03 12:54:47 +0700

    upload code

---
+++ main.js
@@ -0,0 +1,53 @@
+var motNgayLuong = 100000;
+var soNgayLam = 0;
+var  tienLuong = 0;
+
+var soNgayLam = 10;
+
+tienLuong = (motNgayLuong * soNgayLam);
+console.log("Tien luong la:" + tienLuong );
+
+// bai tap 2
+var r1 = 0;
+var r2 = 0;
+var r3 = 0;
+var r4 = 0;
+var r5 = 0;
+var gtTrungBinh = 0;
+// gan gia tri
+var r1 = 1;
+var r2 = 3;
+var r3 = 5;
+var r4 = 6;
+var r5 = 0;
+gtTrungBinh =  (r1+r2+r3+r4+r5) / 5
+console.log("Gia tri trung binh:" + gtTrungBinh);
+// bai tap 3
+var usd = 23500;
+var nhapVao = 0;
+var tienQuyDoi = 0;
+// gan gia tri
+var nhapVao = 50;
+tienQuyDoi = (usd * nhapVao);
+console.log("Tien sau quy doi là:" + tienQuyDoi);
+// bai tap 4
+var chieuDai = 0;
+var chieuRong = 0;
+var chuviHCN = 0;
+var dientichHCN = 0;
+// gan gia tri
+var chieuDai = 9;
+var chieuRong = 5;
+chuviHCN = (chieuDai + chieuRong)*2;
+dientichHCN = (chieuDai *chieuRong);
+console.log ("Chu vi hinh chu nha la:" + chuviHCN);
+console.log ("Dien tich hinh chu nha la:" + dientichHCN);
+// bai tap so 5
+var n = 23
+var hangChuc = Math.floor(n/10);
+var hangDonVi = Math.floor(n%10);
+var tongHaiKySo = hangChuc + hangDonVi;
+console.log("Tong hai ky so:" + tongHaiKySo);
+
+
+
